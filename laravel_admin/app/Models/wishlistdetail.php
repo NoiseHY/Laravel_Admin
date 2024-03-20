ece1b8f08ec1ce1c7ac7827750cc5b1a90d7dba6
NoiseHY <congnamhy1@gmail.com>
2024-03-20 22:35:24 +0700

    Basic model, ctrl, index
    Tạo các file cơ bản, template sẫn sàng

---
+++ laravel_admin/app/Models/wishlistdetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class wishlistdetail extends Model
+{
+    use HasFactory;
+}
